f469f25dd6daf711698ed2664b31d594864a2ef7
Robert Rimoczi <robert@rimoczi.com>
2021-10-19 11:08:14 +0200

    extract auth logic to instagram service

--- src/Controller/RedirectController.php
+++ src/Controller/RedirectController.php
@@ -2,48 +2,25 @@
 
 namespace DcodeGroup\InstagramFeed\Controller;
 
+use DcodeGroup\InstagramFeed\InstagramService;
 use DcodeGroup\InstagramFeed\Profile;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
-use League\OAuth2\Client\Provider\Instagram;
-use League\OAuth2\Client\Provider\InstagramResourceOwner;
-use League\OAuth2\Client\Token\AccessToken;
 
 class RedirectController
 {
-    public function __invoke(Request $request, Instagram $instagramProvider)
+    public function __invoke(Request $request, InstagramService $instagramService)
     {
         // Try to get an access token (using the authorization code grant)
-        $token = $instagramProvider->getAccessToken('authorization_code', [
-            'code' => $request->input('code')
-        ]);
+        $token = $instagramService->getAccessToken($request->input('code'));
 
-        $user = $instagramProvider->getResourceOwner($token);
+        $user = $instagramService->getUser($token);
 
-        // Exchange short-lived token to long-lived
-        $longLivedToken = $this->exchangeToken($instagramProvider, $token);
+        $longLivedToken = $instagramService->acquireLongLivedToken($token);
 
         Profile::create([
             'profile_id' => $user->getId(),
-            'access_token' => $longLivedToken['access_token'],
-            'expires_at' => now()->addSeconds($longLivedToken['expires_in']),
+            'access_token' => $longLivedToken->getToken(),
+            'expires_at' => now()->addSeconds($longLivedToken->getExpires()),
         ]);
-    }
-
-    /**
-     * @param Instagram $instagramProvider
-     * @param AccessToken $token
-     * @return array|mixed
-     *
-     * @TODO: move this method to a service
-     */
-    protected function exchangeToken(Instagram $instagramProvider, AccessToken $token)
-    {
-        return Http::get($instagramProvider->getGraphHost() . '/access_token', [
-            'grant_type' => 'ig_exchange_token',
-            'client_secret' => config('instagram.client_secret'),
-            'access_token' => $token->getToken(),
-        ])
-            ->json();
     }
 }
---
+++ src/InstagramService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace DcodeGroup\InstagramFeed;
+
+use Illuminate\Support\Facades\Http;
+use League\OAuth2\Client\Provider\Instagram;
+use League\OAuth2\Client\Provider\InstagramResourceOwner;
+use League\OAuth2\Client\Token\AccessToken;
+
+class InstagramService
+{
+    private Instagram $provider;
+
+    public function __construct(Instagram $provider)
+    {
+        $this->provider = $provider;
+    }
+
+    public function getAccessToken(string $code): AccessToken
+    {
+        return $this->provider->getAccessToken('authorization_code', [
+            'code' => $code
+        ]);
+    }
+
+    public function getUser(AccessToken $token): InstagramResourceOwner
+    {
+        return $this->provider->getResourceOwner($token);
+    }
+
+    public function acquireLongLivedToken(AccessToken $token): AccessToken
+    {
+        $response = Http::get($this->provider->getGraphHost() . '/access_token', [
+            'grant_type' => 'ig_exchange_token',
+            'client_secret' => config('instagram.client_secret'),
+            'access_token' => $token->getToken(),
+        ])
+            ->json();
+
+        return new AccessToken($response['access_token']);
+    }
+}
