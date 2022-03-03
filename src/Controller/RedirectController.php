f240a92a0723f61b44b1d2836775afa175d1aa5f
set <set>
2022-03-03 09:19:59 +0100

    if an error happened during the redirect, redirect to error config

--- src/Controller/RedirectController.php
+++ src/Controller/RedirectController.php
@@ -11,6 +11,12 @@ class RedirectController
 {
     public function __invoke(Request $request, InstagramAuthService $instagramService)
     {
+        if ($request->has('error')) {
+            return redirect()->route(
+                config('instagram.redirects.error'),
+                ['message' => $request->input('error_description', __('Unknown error occured'))]
+            );
+        }
         // Try to get an access token (using the authorization code grant)
         $token = $instagramService->acquireAccessTokenFromCode($request->input('code'));
 
@@ -24,6 +30,9 @@ class RedirectController
             'expires_at' => Carbon::createFromTimestamp($longLivedToken->getExpires()),
         ]);
 
-        return redirect()->route(config('instagram.redirects.success'));
+        return redirect()->route(
+            config('instagram.redirects.success'),
+            ['message' => __('Successfully connected the Instagram profile')]
+        );
     }
 }
